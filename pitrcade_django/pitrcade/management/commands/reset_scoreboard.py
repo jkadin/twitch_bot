ddb2df96338775598195d5962745968bba59847c
jkadin <jkadin>
2020-06-23 19:11:02 -0700

    Fix external script access to preferences model

--- pitrcade_django/pitrcade/management/commands/reset_scoreboard.py
+++ pitrcade_django/pitrcade/management/commands/reset_scoreboard.py
@@ -11,5 +11,6 @@ class Command(BaseCommand):
         now = timezone.now()
         if now >= reset_datetime:
             Player.objects.all().delete()
-            preferences.ConfigSetting.scoreboard_next_player_reset = (reset_datetime + preferences.ConfigSetting.scoreboard_player_reset_interval)
-            preferences.ConfigSetting.save()
+            cs = ConfigSetting.objects.get(pk=preferences.ConfigSetting.pk)
+            cs.scoreboard_next_player_reset = reset_datetime + preferences.ConfigSetting.scoreboard_player_reset_interval
+            cs.save()
